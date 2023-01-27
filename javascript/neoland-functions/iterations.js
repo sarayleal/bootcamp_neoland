64014ef9b5142d1203a2d851ee53fa5e12d3da16
saray leal <sarayleal2022@gmail.com>
2023-01-27 22:31:23 +0100

    Functions

--- javascript/neoland-functions/iterations.js
+++ javascript/neoland-functions/iterations.js
@@ -40,109 +40,119 @@ console.log(findLongestWord(avengers));
 /* Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
 Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz.  */
 
-const numbers = [1, 2, 3, 5, 45, 37, 58];
-
-function sumAll(param) {
-  let sum = 0;
-  for (let i = 0; i < param.length; i++) {
-    sum += param[i];
-  }
-  return sum;
-}
-
-console.log(sumAll(numbers));
-
-
+/* const numbers = [1, 2, 3, 5, 45, 37, 58];
+const sumAll = (param) => {
+  let sumAll = 0;
+  for (let i = 0; i < numbers.length; i++);
+  sumAll += param[i];
+};
+sumAll(numbers);
+console.log(sumAll); */
 
 /* **Iteración #4: Calcular el promedio**
 
 Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función:
  */
-const numbers = [12, 21, 38, 5, 45, 37, 6];
+/* const numbers = [12, 21, 38, 5, 45, 37, 6];
 function average(param) {
   // insert code
 }
-
-
-
-
-
+ */
 
 /* Iteración #5: Calcular promedio de strings**
 
 Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función: */
 
-const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+/* const mixedElements = [6, 1, "Rayo", 1, "vallecano", "10", "upgrade", 8, "hub"];
+let stringContainer = [mixedElements]
+let numberContainer = [mixedElements]
 function averageWord(param) {
-  // insert code
+  for (let i=0; i<param.length; i++) {
+    if ( typeof param[i] == "string") {
+      stringContainer += param[i].length
+    }else {numberContainer += param[i]}
+  }
+
 }
+console.log(stringContainer);
+console.log(numberContainer); */
 
 
 /* **Iteración #6: Valores únicos**
 
 Crea una función que reciba por parámetro un array y compruebe si existen elementos duplicados, en caso que existan los elimina para retornar un array sin los elementos duplicados. Puedes usar este array para probar tu función: */
 
-
 const duplicates = [
-  'sushi',
-  'pizza',
-  'burger',
-  'potatoe',
-  'pasta',
-  'ice-cream',
-  'pizza',
-  'chicken',
-  'onion rings',
-  'pasta',
-  'soda'
+  "sushi",
+  "pizza",
+  "burger",
+  "potatoe",
+  "pasta",
+  "ice-cream",
+  "pizza",
+  "chicken",
+  "onion rings",
+  "pasta",
+  "soda",
 ];
 function removeDuplicates(param) {
-  // insert code
+ const remove = []
+ for (let i=0; i<param.length; i++) {
+  if (!remove.includes(param[i])) {
+    remove.push(param[i])
+  }
+ }
+ console.log(remove)
 }
-
-
-
-
-
+removeDuplicates(duplicates)
 /* **Iteración #7: Buscador de nombres**
 
 Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función: */
 
-
 const nameFinder = [
-  'Peter',
-  'Steve',
-  'Tony',
-  'Natasha',
-  'Clint',
-  'Logan',
-  'Xabier',
-  'Bruce',
-  'Peggy',
-  'Jessica',
-  'Marc'
+  "Peter",
+  "Steve",
+  "Tony",
+  "Natasha",
+  "Clint",
+  "Logan",
+  "Xabier",
+  "Bruce",
+  "Peggy",
+  "Jessica",
+  "Marc",
 ];
-function finderName(param) {
-  // insert code
+function finderName(param, value) {
+  let found = false;
+  let position;
+  for ( let i=0; i<param.length; i++) {
+    if (param[i] === value) {
+      found = true;
+      position = i;
+      break;
 }
-
+  }
+  return {found: found, position: position};
+}
+console.log(finderName(nameFinder, "Xabier"));
+console.log(finderName(nameFinder, "Saray"));
 
 /* **Iteration #8: Contador de repeticiones**
 
 Crea una función que nos devuelva el número de veces que se repite cada una de las palabras que lo conforma.  Puedes usar este array para probar tu función: */
 
 const counterWords = [
-  'code',
-  'repeat',
-  'eat',
-  'sleep',
-  'code',
-  'enjoy',
-  'sleep',
-  'code',
-  'enjoy',
-  'upgrade',
-  'code'
+  "code",
+  "repeat",
+  "eat",
+  "sleep",
+  "code",
+  "enjoy",
+  "sleep",
+  "code",
+  "enjoy",
+  "upgrade",
+  "code",
 ];
 function repeatCounter(param) {
   // insert code
