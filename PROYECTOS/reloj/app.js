7570d946dc5cfbb4451a22725b1bbb0b34410e2b
saray leal <sarayleal2022@gmail.com>
2023-01-28 00:24:26 +0100

    reloj

--- PROYECTOS/reloj/app.js
+++ PROYECTOS/reloj/app.js
@@ -1,8 +1,28 @@
-setInterval (() => {
+/* setInterval (() => {
     const date = newDate();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
     console.log(`${hours}:${minutes}:${seconds}`);
-}, 1000);
+}, 1000); */
+
+function updateClock() {
+    let currentTime = new Date();
+    let currentHours = currentTime.getHours();
+    let currentMinutes = currentTime.getMinutes();
+    let currentSeconds = currentTime.getSeconds();
+
+    currentMinutes = (currentMinutes < 10 ? "0" : "") + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? "0" : "") + currentSeconds;
+
+    let timeOfDay = (currentHours < 12) ? "AM" : "PM";
+    currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
+    currentHours = (currentHours == 0) ? 12 : currentHours;
+
+    let currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
+
+    document.getElementById("clock").innerHTML = currentTimeString;
+  }
+
+  setInterval(updateClock, 1000);
 
