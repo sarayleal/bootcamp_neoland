e0b111752c34cb963f66ed99d1fde26a55ccd8e5
saray leal <sarayleal2022@gmail.com>
2023-01-29 18:35:09 +0100

    es6 ietration1

--- javascript/ejercicios/neoland-es6/iteration1.js
+++ javascript/ejercicios/neoland-es6/iteration1.js
@@ -1,18 +1,27 @@
 /* Crea una arrow function que tenga dos parametros a y b y 
 que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre 
-por consola la suma de los dos parametros.
+por consola la suma de los dos parametros. */
 
-1.1 Ejecuta esta función sin pasar ningún parametro
-1.2 Ejecuta esta función pasando un solo parametro
-1.3 Ejecuta esta función pasando dos parametros */
+let a = 10;
+let b = 5;
+
+/* 1.1 Ejecuta esta función sin pasar ningún parametro */
 const sum = () => {
-  return a + b;
-};
-const sum2 = (a = 10) => {
-  return a + b;
-};
-const sum3 = (a = 10, b = 5) => {
-  return a + b;
-};
+  return(" ");
+}
+console.log(sum)
+
+/* 1.2 Ejecuta esta función pasando un solo parametro */
+const sum2 = (a) => {
+  return(a);
+}
+console.log(sum2)
+
+/* 1.3 Ejecuta esta función pasando dos parametros  */
+const sum3 = (a , b) => {
+  return (a + b);
+}
+console.log(sum3)
+
 
 
